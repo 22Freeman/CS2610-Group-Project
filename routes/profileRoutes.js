99e3250ca94cc144430607687280f6aea064d06b
Tanner Nielson <tannerclarknielson@gmail.com>
2015-11-13 12:50:26 -0700

    profile picture updated and route fixed

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/profile', function(req, res){
+router.get('/', function(req, res){
   res.render('profile', {
     //title:'Googlegram+'
   })
