5477c426ce07bb13770d75fe9ee50365ab9b5f02
ncksta <mail@nickstauffer.com>
2015-11-13 16:03:40 -0700

    redirect profile if not authenticated

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -2,10 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res){
+  if (ACCESS_TOKEN == "")
+  {
+  res.redirect('/../');
+  }
+  else
+  {
   res.render('profile', {
     title:'Googlegram+',
     user: username
   })
+}
 });
 
 module.exports = router
