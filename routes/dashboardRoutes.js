629ac40c81eb4c9d95d54f5338f67983f7a14a37
ncksta <mail@nickstauffer.com>
2015-11-13 12:48:46 -0700

    changed route stuff

--- routes/dashboardRoutes.js
+++ routes/dashboardRoutes.js
@@ -10,14 +10,6 @@ router.get('/', function(req, res){
   }
   else
   {
-    res.render('dashboard', {
-      title:'Googlegram+',
-      name: username
-    })
-  }
-});
-
-router.post('/',function(req,res){
   request({
     url:'https://api.instagram.com/v1/users/self/feed?access_token='+ACCESS_TOKEN}, function (error, response, body) {
     if (!error && response.statusCode == 200) {
@@ -25,35 +17,34 @@ router.post('/',function(req,res){
       res.render('dashboard', {
         title: 'Googlegram+',
         name: username,
-        URL1: feed.data.images.standard_resolution.url,
-        URL2: feed.data.images.standard_resolution.url,
-        URL3: feed.data.images.standard_resolution.url,
-        URL4: feed.data.images.standard_resolution.url,
-        URL5: feed.data.images.standard_resolution.url,
-        URL6: feed.data.images.standard_resolution.url,
-        link1: feed.data.link,
-        link2: feed.data.link,
-        link3: feed.data.link,
-        link4: feed.data.link,
-        link5: feed.data.link,
-        link6: feed.data.link,
-        likes1: feed.data.likes.count,
-        likes2: feed.data.likes.count,
-        likes3: feed.data.likes.count,
-        likes4: feed.data.likes.count,
-        likes5: feed.data.likes.count,
-        likes6: feed.data.likes.count,
-        comments1: feed.data.comments.count,
-        comments2: feed.data.comments.count,
-        comments3: feed.data.comments.count,
-        comments4: feed.data.comments.count,
-        comments5: feed.data.comments.count,
-        comments6: feed.data.comments.count,
-
-
+        URL1: feed.data[0].images.standard_resolution.url,
+        URL2: feed.data[1].images.standard_resolution.url,
+        URL3: feed.data[2].images.standard_resolution.url,
+        URL4: feed.data[3].images.standard_resolution.url,
+        URL5: feed.data[4].images.standard_resolution.url,
+        URL6: feed.data[5].images.standard_resolution.url,
+        link1: feed.data[0].link,
+        link2: feed.data[1].link,
+        link3: feed.data[2].link,
+        link4: feed.data[3].link,
+        link5: feed.data[4].link,
+        link6: feed.data[5].link,
+        likes1: feed.data[0].likes.count,
+        likes2: feed.data[1].likes.count,
+        likes3: feed.data[2].likes.count,
+        likes4: feed.data[3].likes.count,
+        likes5: feed.data[4].likes.count,
+        likes6: feed.data[5].likes.count,
+        comments1: feed.data[0].comments.count,
+        comments2: feed.data[1].comments.count,
+        comments3: feed.data[2].comments.count,
+        comments4: feed.data[3].comments.count,
+        comments5: feed.data[4].comments.count,
+        comments6: feed.data[5].comments.count,
       });
     }
   })
-});
 
+}
+});
 module.exports = router
