087c1e7dee99e19cf4c47ffd18a0d46bbe0f8a27
Tanner Nielson <tannerclarknielson@gmail.com>
2015-12-10 15:05:53 -0700

    profile changes for database

--- routes/profileRoutes.js
+++ routes/profileRoutes.js
@@ -1,5 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var cfg = require('../config.js')
+var Users = require('../models/users')
+var querystring = require('querystring')
+var bodyParser = require('body-parser')
+var request = require('request')
+var session = require('express-session')
+
+router.use(bodyParser.urlencoded({extended: false}))
+
+router.use(session({
+   cookieName: 'session',
+   secret: 'asdfasd;lkjweiovnxc,kmeuinf',
+   resave: false,
+   saveUninitialized: true
+}))
 
 router.get('/', function(req, res){
   if (req.session.access_token == undefined)
@@ -14,5 +29,38 @@ router.get('/', function(req, res){
     })
   }
 });
+
+
+router.post('/', function(req,res) {
+      var user = req.body
+      Users.insert( user, function(result){
+         req.session.userId = result.ops[0]._id
+         res.redirect('/update')
+      })
+})
+
+router.get('/update', function(req,res) {
+   if(req.session.userId) {
+      Users.find(req.session.userId, function(document) {
+         if(!document) return res.redirect('/')
+         res.render('update', {
+            user: document
+         })
+      })
+   }
+   else {
+      res.redirect('/')
+   }
+})
+
+router.post('/update', function(req,res) {
+   var user = req.body
+   Users.update(user, function(result) {
+      res.render('update', {
+         user:user,
+         success: 'User updated successfully'
+      })
+   })
+})
 
 module.exports = router
